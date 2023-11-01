bc02667c6e4e10bc148749543c5b799f93d6767b
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-11-01 18:27:09 +0800

    add drawer

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:navigation/second_screen.dart';
 
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('First Screen')),
-
+      appBar: AppBar(title: const Text('First Screen')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Navigation'),
+            ),
+            ListTile(
+                selected: true,
+                title: const Text('First Screen'),
+                onTap: () {
+                  Navigator.pop(context);
+                }),
+            ListTile(
+              selected: false,
+              title: const Text('Second Screen'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             // Navigate to the second screen.
-            Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+            Navigator.pushNamed(context, '/second');
           },
-          child: Text('Go to Second Screen'),
+          child: const Text('Go to Second Screen'),
         ),
       ),
     );
