37be925a7e1ca04602027111f9d979b2b561fba7
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-11-01 18:37:50 +0800

    add drawer

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -4,14 +4,41 @@ class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Second Screen')),
+      appBar: AppBar(title: const Text('Second Screen')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Navigation'),
+            ),
+            ListTile(
+                selected: false,
+                title: const Text('First Screen'),
+                onTap: () {
+                  Navigator.pop(context);
+                Navigator.pushNamed(context, '/');
+                }),
+            ListTile(
+              selected: true,
+              title: const Text('Second Screen'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             // Return to the first screen.
-            Navigator.pop(context);
+            Navigator.pushNamed(context, '/');
           },
-          child: Text('Go Back to First Screen'),
+          child: const Text('Go Back to First Screen'),
         ),
       ),
     );
